a41c68895262985b9b8b9a65a8867c5783fe940f
TechTheAwesome <techtheawesome01@gmail.com>
2023-05-05 15:48:10 +0900

    ADD dark mode toggle switch

--- src/app/clientComponents.tsx
+++ src/app/clientComponents.tsx
@@ -1,14 +1,18 @@
 "use client";
 import { useState } from "react";
-
+import { Switch } from "@/components/ui/switch";
 
 export function DarkModeToggle() {
 	const [dark, setDark] = useState(false);
 	return <div className="ml-4">
-		<input type="checkbox" role="switch" checked={dark} 
+		<Switch id="dark-mode-switch" checked={dark} onCheckedChange={(checked) => {
+			document.documentElement.className = checked ? "dark" : "";
+			setDark(checked);
+		}} />
+		{/* <input type="checkbox" role="switch" checked={dark} 
 		onChange={() =>{
 			document.documentElement.className = !dark? "dark" : "";
 			setDark(!dark);
-		}} />
+		}} /> */}
 	</div>;
 }
